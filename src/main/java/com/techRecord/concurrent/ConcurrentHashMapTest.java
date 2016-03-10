e6000c323d6256678debac4504a3ad69a472dd90
anytimekaka@163.com <wang.guisheng@sfit.shfe.com.cn>
2016-03-10 11:49:18 +0800

    update

--- src/main/java/com/techRecord/concurrent/ConcurrentHashMapTest.java
+++ src/main/java/com/techRecord/concurrent/ConcurrentHashMapTest.java
@@ -4,10 +4,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ConcurrentHashMapTest {
 
-	private static ConcurrentHashMap<Integer,Integer> map = new ConcurrentHashMap<Integer, Integer>();
-	
 	public static void main(String args[]){
+		
+		ConcurrentHashMap<Integer,Integer> map = new ConcurrentHashMap<Integer, Integer>(15, 0.75f, 14);
 		map.put(1, 1);
+		map.put(5, 5);
+		map.put(7, 7);
+		map.put(2, null);
+		
 		System.out.println(15>>>2);
 		System.out.println(15>>2);
 		
